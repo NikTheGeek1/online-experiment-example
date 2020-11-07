de33b2bbc1398f9dd27f91bc0138de659ed5e3d8
Nikos Theodoropoulos <30304263+NikTheGeek1@users.noreply.github.com>
2020-11-07 14:27:53 +0200

    Update passenger_wsgi.py

--- passenger_wsgi.py
+++ passenger_wsgi.py
@@ -2,9 +2,20 @@ import os
 import sys
 
 from app import create_app
+from flask_script import Manager, Shell
 
+from app import create_app
 
-sys.path.insert(0, os.path.dirname(__file__))
+sys.path.insert(0, os.path.dirname(_file_))
 
+application = create_app(os.getenv('FLASK_ENV') or 'config.ProductionConfig')
+manager = Manager(application)
 
-application = create_app('config.ProductionConfig')
+# these names will be available inside the shell without explicit import
+def make_shell_context():
+    return dict(app=application)
+
+manager.add_command('shell', Shell(make_context=make_shell_context))
+
+if _name_ == '_main_':
+    manager.run()
